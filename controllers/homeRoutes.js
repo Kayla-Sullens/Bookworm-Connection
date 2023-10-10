828c685fafb0bd56ea3cac1f933967e83a4131a7
Sal Ceja <salceja45@gmail.com>
2023-10-10 13:09:05 -0700

    carousel template layout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,27 +1,29 @@
-const router = require('express').Router();
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const testData = require("../seeds/testingData_myBooks");
+const withAuth = require("../utils/auth");
 
 // Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
-    res.render('homepage', {
+    res.render("homepage", {
       user: req.session.user,
       // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
+      testData: testData,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If a session exists, redirect the request to the homepage
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
