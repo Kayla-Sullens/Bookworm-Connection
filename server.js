2b2dec6e2b3424caa8f671df7df28eb16dc613b0
Nefta007 <nperc001@ucr.edu>
2023-10-16 21:19:56 -0700

    added a few more comments explaing code, as well as anding a link

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 
-const sequelize = require("./config/connection");
+const sequelize = require("./config/connection"); //checking for JawsDb url since we are using heroku
 
 // Create a new sequelize store using the express-session package
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
